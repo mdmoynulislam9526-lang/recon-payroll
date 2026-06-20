4ca4b2ba6591018950a4ef3428d93632874812ee
Md Moynul Islam <mdmoynulislam9526@gmail.com>
2026-06-20 13:59:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -189,11 +189,11 @@ with col2:
     conn.close()
     
     if rows:
-# --- মাস এবং বছর সিলেক্ট করার আপডেট করা লজিক ---
-    if 'selected_month' not in st.session_state:
-    st.session_state.selected_month = datetime.now().strftime("%B") # এটি অবশ্যই এক ট্যাব ডানদিকে হবে
-
 months_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+# সেশন স্টেটে মাসটি সেভ করার ব্যবস্থা
+    if 'selected_month' not in st.session_state:
+    st.session_state.selected_month = datetime.now().strftime("%B")
 
 c_col1, c_col2 = st.columns(2)
 
