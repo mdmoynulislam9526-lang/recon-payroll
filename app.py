d25af2cf78ec2ea8bf20b3bdf0806108c0d430b7
Md Moynul Islam <mdmoynulislam9526@gmail.com>
2026-06-24 18:32:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ net_final = net_p + total_ot_emp + rec['bonus_amount']
 sig_html_element = f"<img src='data:image/png;base64,{sig_base64_str}' style='width: 150px;'>" if sig_base64_str else "____________________"
 st.markdown(sig_html_element, unsafe_allow_html=True)
             # Pay Slip HTML Design
-            payslip_preview_html = f"""
+payslip_preview_html = f"""
             <div style="font-family: 'Segoe UI', Arial, sans-serif; padding: 35px; background: white; color: black; border: 1px solid #c8d6e5; border-radius: 8px; max-width: 700px; margin: 15px auto; box-shadow: 0 4px 20px rgba(0,0,0,0.06);">
                 <div style="text-align: center; margin-bottom: 25px; padding-bottom: 15px; border-bottom: 3px solid #1F4E78;">
                     {"<img src='data:image/png;base64," + logo_base64_str + "' style='max-height: 65px;'>" if logo_base64_str else ""}
