0da41c947cf7e164f3b56490357ce846cbf81821
Md Moynul Islam <mdmoynulislam9526@gmail.com>
2026-06-24 18:25:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ total_ot_emp = rec['overtime_hours'] * rec['overtime_rate']
 net_final = net_p + total_ot_emp + rec['bonus_amount']
 
             # সেই আগের সিগনেচার লজিক
-            sig_html_element = f"<img src='data:image/png;base64,{sig_base64_str}' style='width: 150px;'>" if sig_base64_str else "____________________"
+              sig_html_element = f"<img src='data:image/png;base64,{sig_base64_str}' style='width: 150px;'>" if sig_base64_str else "____________________"
 
             # Pay Slip HTML Design
             payslip_preview_html = f"""
