6f626d0657931e7c88cda04f012c1490f3fd5812
Md Moynul Islam <mdmoynulislam9526@gmail.com>
2026-06-25 17:54:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ for cat_name, title_text in zip(categories_list, display_titles):
                     eid, name, desg, cat, dept, base_sal = r
                     rec = saved_db_tracker.get(str(eid), {"present": days_in_month if cat == 'Worker (Daily Basis)' else 26, "absent": 0, "fine": 0.0, "ot_hrs": 0.0, "ot_rate": 0.0, "bonus": 0.0, "advance": 0.0})
                     
-                    gross, house_rent, medical, _, ab_cut, net_p, adv_paid = calculate_salary_breakdown(base_sal, rec['absent_days'], rec['fine'], cat, rec['present_days'], rec['advance_cut'])
+                    gross, house_rent, medical, _, ab_cut, net_p, adv_paid = calculate_salary_breakdown(base_sal, rec['absent_days'], rec['fine_cut'], cat, rec['present_days'], rec['advance_cut'])
                     ot_total = rec['ot_hrs'] * rec['ot_rate']
                     final_payable = net_p + ot_total + rec['bonus']
                     
